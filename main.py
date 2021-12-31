7436a659fd154d0ac5c4f1b8136bea59c8114369
Yoriz <dave.wilson1@ntlworld.com>
2021-12-31 09:09:00 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from db.init_db import init_engine, init_session_maker
+from db.create_db import create_db
+from db.tables import mapper_registry
+from db.add_test_data import add_test_data
+
+CREATE_DB = True
+ADD_TEST_DATA = True
+DB_URL = "sqlite:///hotel/hotel.db"
+
+
+def main():
+    engine = init_engine(url=DB_URL)
+    SessionMaker = init_session_maker(engine=engine)
+    if CREATE_DB:
+        create_db(engine=engine, mapper_registry=mapper_registry)
+    if ADD_TEST_DATA:
+        add_test_data(session_maker=SessionMaker)
+
+
+if __name__ == "__main__":
+    main()
